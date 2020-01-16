74fc1bc9a7ceb54ce2c4c1389b946361b0384775
Andrzej Popek <andrzejpopek93@gmail.com>
2020-01-16 15:14:40 +0100

    add tests, add grid

--- src/components/features/Feedback/Feedback.js
+++ src/components/features/Feedback/Feedback.js
@@ -1,9 +1,19 @@
 import React from 'react';
-// import styles from './Feedback.module.scss';
+import styles from './Feedback.module.scss';
 // import PropTypes from 'prop-types';
 
 const Feedback = () => {
-  return <h1>Hello, Feedback here!</h1>;
+  return (
+    <div className={styles.root}>
+      <div className='container'>
+        <div className='row'>
+          <div className='col'>
+            <h1>Hello, Feedback here!</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Feedback;
