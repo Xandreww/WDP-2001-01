3acfa23920448f28c41f4c048fc23330661a52cc
Andrzej Popek <andrzejpopek93@gmail.com>
2020-01-16 14:54:10 +0100

    init Feedback

---
+++ src/components/features/Feedback/Feedback.js
@@ -0,0 +1,9 @@
+import React from 'react';
+// import styles from './Feedback.module.scss';
+// import PropTypes from 'prop-types';
+
+const Feedback = () => {
+  return <h1>Hello, Feedback here!</h1>;
+};
+
+export default Feedback;
