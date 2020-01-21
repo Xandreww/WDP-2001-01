331b4f3e4f75433c86bb5d09a6e938b59e9e73b6
Rado Jedro <radoslaw.jedro@gmail.com>
2020-01-21 01:59:54 +0000

    Update Promoted.js

--- src/components/features/Promoted/Promoted.js
+++ src/components/features/Promoted/Promoted.js
@@ -5,7 +5,56 @@ import PropTypes from 'prop-types';
 import Button from '../../common/Button/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import PromotedProductBox from '../../common/PromotedProductBox/PromotedProductBoxContainer';
+import HotDealsProductBox from '../../common/HotDealsProductBox/HotDealsProductBoxContainer';
 
+const Promoted = () => {
+  return (
+    <div className={styles.root}>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-4'>
+            <div className={styles.promotedBar}>
+              <h4 className={`col-6 ${styles.promotedBarTitle}`}>HOT DEALS</h4>
+              <p className={`col-6 text-right ${styles.dots}`}>
+                <a href='#' className={styles.dot}>
+                  <span></span>
+                </a>
+                <a href='#' className={styles.dot}>
+                  <span></span>
+                </a>
+                <a href='#' className={styles.dot}>
+                  <span></span>
+                </a>
+              </p>
+            </div>
+            <HotDealsProductBox />
+          </div>
+          <div className='col-8'>
+            <PromotedProductBox />
+            <div className={styles.btns}>
+              <Button className={styles.btn}>
+                <FontAwesomeIcon icon={faAngleLeft}></FontAwesomeIcon>
+              </Button>
+              <Button className={styles.btn}>
+                <FontAwesomeIcon icon={faAngleRight}></FontAwesomeIcon>
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Promoted.propTypes = {
+  promotedProductBox: PropTypes.object,
+  hotDealsProductBox: PropTypes.object,
+};
+
+export default Promoted;
+
+/*
 const Promoted = ({ promoted }) => {
   return (
     <div className={styles.root}>
@@ -97,4 +146,4 @@ Promoted.propTypes = {
   promoted: PropTypes.object,
 };
 
-export default Promoted;
+*/
