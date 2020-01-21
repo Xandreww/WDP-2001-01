948f5be572eab509000b6e64b46cb629bff61b4d
Rado Jedro <radoslaw.jedro@gmail.com>
2020-01-21 01:58:25 +0000

    Update Homepage with Promoted component

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -8,11 +8,9 @@ import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
 import SaleContainer from '../../features/Sale/SaleContainer';
 import Feedback from '../../features/Feedback/FeedbackContainer';
 import Brands from '../../features/Brands/BrandsContainer';
-import HotDealsProductBox from '../../common/HotDealsProductBox/HotDealsProductBox';
 
 const Homepage = () => (
   <div className={styles.root}>
-    <HotDealsProductBox />
     <Promoted />
     <FeatureBoxes />
     <SaleContainer />
