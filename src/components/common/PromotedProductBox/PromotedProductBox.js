994585da1a980ecbdf797264fc164eeeffb4b804
Rado Jedro <radoslaw.jedro@gmail.com>
2020-01-21 02:00:12 +0000

    Update PromotedProductBox.js

--- src/components/common/PromotedProductBox/PromotedProductBox.js
+++ src/components/common/PromotedProductBox/PromotedProductBox.js
@@ -3,22 +3,16 @@ import PropTypes from 'prop-types';
 import Button from '../Button/Button';
 import styles from './PromotedProductBox.module.scss';
 
-const PromotedProductBox = ({
-  image,
-  imageName,
-  softTitle,
-  strongTitle,
-  description,
-}) => {
+const PromotedProductBox = ({ promotedProductBox }) => {
   return (
     <div className={styles.root}>
-      <img src={image} alt={imageName} />
+      <img src={promotedProductBox.image} alt={promotedProductBox.imageName} />
       <div className={styles.text}>
         <h3 className={styles.title}>
-          {softTitle}
-          <strong> {strongTitle}</strong>
+          {promotedProductBox.softTitle}
+          <strong> {promotedProductBox.strongTitle}</strong>
         </h3>
-        <p className={styles.description}>{description}</p>
+        <p className={styles.description}>{promotedProductBox.description}</p>
       </div>
       <Button className={styles.mainBtn}>Shop now</Button>
     </div>
@@ -26,11 +20,7 @@ const PromotedProductBox = ({
 };
 
 PromotedProductBox.propTypes = {
-  image: PropTypes.string,
-  imageName: PropTypes.string,
-  softTitle: PropTypes.string,
-  strongTitle: PropTypes.string,
-  description: PropTypes.string,
+  promotedProductBox: PropTypes.object,
 };
 
 export default PromotedProductBox;
