ae063f928dcac4130cddb73376ecc1ed772eb75f
Marcin Dzieszkowski <m.dzieszkowski@gmail.com>
2020-01-22 11:36:05 +0100

    add responsive to panelbar

--- src/components/features/NewFurniture/NewFurniture.js
+++ src/components/features/NewFurniture/NewFurniture.js
@@ -99,7 +99,7 @@ class NewFurniture extends React.Component {
       <div className={styles.root}>
         <div className='container'>
           <div className={styles.panelBar}>
-            <div className='row no-gutters align-items-end'>
+            <div className={'row no-gutters align-items-end ' + styles.panelBarElement}>
               <div className={'col-auto ' + styles.heading}>
                 <h3>New furniture</h3>
               </div>
