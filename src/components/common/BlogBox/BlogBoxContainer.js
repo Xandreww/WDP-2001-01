6302ff38e7a81eb4cef88d9a18efb938dc789ca3
Rado Jedro <radoslaw.jedro@gmail.com>
2020-01-23 00:54:16 +0000

    Add new  blogBox1,2,3 props

--- src/components/common/BlogBox/BlogBoxContainer.js
+++ src/components/common/BlogBox/BlogBoxContainer.js
@@ -3,7 +3,9 @@ import { connect } from 'react-redux';
 import BlogBox from './BlogBox';
 
 const mapStateToProps = state => ({
-  blogBox: state.blogBox,
+  blogBox1: state.blogBox1,
+  blogBox2: state.blogBox2,
+  blogBox3: state.blogBox3,
 });
 
 export default connect(mapStateToProps)(BlogBox);
