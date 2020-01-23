310c8a14f60d1fedb0e2873aa55bb70a6a767aaf
Rado Jedro <radoslaw.jedro@gmail.com>
2020-01-23 00:13:59 +0000

    Add dots to panelBar

--- src/components/features/LatestBlog/LatestBlog.js
+++ src/components/features/LatestBlog/LatestBlog.js
@@ -12,6 +12,20 @@ const LatestBlog = blogBox => {
             <div className={'col-auto ' + styles.heading}>
               <h3>Latest Blog</h3>
             </div>
+            <div className={'col ' + styles.menu}></div>
+            <div className={'col-auto ' + styles.dots}>
+              <ul>
+                <li>
+                  <a href='#0'> </a>
+                </li>
+                <li>
+                  <a href='#1'> </a>
+                </li>
+                <li>
+                  <a href='#2'> </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
         <div className={styles.blogBox}>
