b6c9c1eec089e2dd804ff33740c5a2c748816d7a
Andrzej Popek <andrzejpopek93@gmail.com>
2020-01-24 18:03:18 +0100

    add items to display

--- src/components/layout/ProductListBar/ProductListBar.js
+++ src/components/layout/ProductListBar/ProductListBar.js
@@ -1,14 +1,55 @@
 import React from 'react';
 import styles from './ProductListBar.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faThLarge, faList } from '@fortawesome/free-solid-svg-icons';
 
 const ProductListBar = () => (
   <div className={styles.root}>
     <div className='container'>
-      <div className={`row + ${styles.row}`}>
-        <div className={`col-4`}>Furniture</div>
-        <div className={`col-4`}>Sort by: Price</div>
-        <div className={`col-2`}>show 12</div>
-        <div className={`col-2`}>icons</div>
+      <div className={'row ' + styles.row}>
+        <div className={'col-4'}>
+          <h2 className={styles.header}>Furniture</h2>
+        </div>
+        <div className={'col-4 ' + styles.colTwo}>
+          <p>Sort By</p>
+          <div className={styles.dropdown}>
+            <button>Price: Lowest first</button>
+            <ul>
+              <li>
+                <a href='#'>Price: Highest first</a>
+              </li>
+              <li>
+                <a href='#'>Rating: Highest first</a>
+              </li>
+              <li>
+                <a href='#'>Rating: Lowest first</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className={'col-2 ' + styles.colThree}>
+          <p>Show</p>
+          <div className={styles.dropdown}>
+            <button>12</button>
+            <ul>
+              <li>
+                <a href='#'>24</a>
+              </li>
+              <li>
+                <a href='#'>48</a>
+              </li>
+              <li>
+                <a href='#'>64</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className={'col-2 ' + styles.colFour}>
+          <div className={styles.icons}>
+            <FontAwesomeIcon className={styles.icon} icon={faThLarge}></FontAwesomeIcon>
+            <FontAwesomeIcon className={styles.icon} icon={faList}></FontAwesomeIcon>
+          </div>
+        </div>
       </div>
     </div>
   </div>
