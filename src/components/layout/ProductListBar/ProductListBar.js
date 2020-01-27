09a42f513f47f4fdc7a7a74a5cd85cab5281bd97
Andrzej Popek <andrzejpopek93@gmail.com>
2020-01-27 08:47:41 +0100

    add styles

--- src/components/layout/ProductListBar/ProductListBar.js
+++ src/components/layout/ProductListBar/ProductListBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './ProductListBar.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faThLarge, faList } from '@fortawesome/free-solid-svg-icons';
+import { faThLarge, faList, faCaretDown } from '@fortawesome/free-solid-svg-icons';
 
 const ProductListBar = () => (
   <div className={styles.root}>
@@ -11,9 +11,15 @@ const ProductListBar = () => (
           <h2 className={styles.header}>Furniture</h2>
         </div>
         <div className={'col-4 ' + styles.colTwo}>
-          <p>Sort By</p>
+          <p className={styles.description}>Sort By</p>
           <div className={styles.dropdown}>
-            <button>Price: Lowest first</button>
+            <button>
+              <p>Price: Lowest first</p>
+              <FontAwesomeIcon
+                className={styles.dropdownIconOne}
+                icon={faCaretDown}
+              ></FontAwesomeIcon>
+            </button>
             <ul>
               <li>
                 <a href='#'>Price: Highest first</a>
@@ -28,9 +34,15 @@ const ProductListBar = () => (
           </div>
         </div>
         <div className={'col-2 ' + styles.colThree}>
-          <p>Show</p>
+          <p className={styles.description}>Show</p>
           <div className={styles.dropdown}>
-            <button>12</button>
+            <button>
+              <p>12</p>
+              <FontAwesomeIcon
+                className={styles.dropdownIconTwo}
+                icon={faCaretDown}
+              ></FontAwesomeIcon>
+            </button>
             <ul>
               <li>
                 <a href='#'>24</a>
