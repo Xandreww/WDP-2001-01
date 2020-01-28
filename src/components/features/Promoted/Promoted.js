7e88db718b5c22b61085806c8305ee937fe5bafb
Rado Jedro <radoslaw.jedro@gmail.com>
2020-01-28 23:28:39 +0000

    Change to class component

--- src/components/features/Promoted/Promoted.js
+++ src/components/features/Promoted/Promoted.js
@@ -8,44 +8,51 @@ import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 import PromotedProductBox from '../../common/PromotedProductBox/PromotedProductBoxContainer';
 import HotDealsProductBox from '../../common/HotDealsProductBox/HotDealsProductBoxContainer';
 
-const Promoted = () => {
-  return (
-    <div className={styles.root}>
-      <div className='container'>
-        <div className='row'>
-          <div className='col-4'>
-            <div className={styles.promotedBar}>
-              <h4 className={`col-6 ${styles.promotedBarTitle}`}>HOT DEALS</h4>
-              <p className={`col-6 text-right ${styles.dots}`}>
-                <a href='#' className={styles.dot}>
-                  <span></span>
-                </a>
-                <a href='#' className={styles.dot}>
-                  <span></span>
-                </a>
-                <a href='#' className={styles.dot}>
-                  <span></span>
-                </a>
-              </p>
+class Promoted extends React.Component {
+  state = {
+    activePage: 0,
+    activeCategory: 'bed',
+  };
+
+  render() {
+    return (
+      <div className={styles.root}>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-4'>
+              <div className={styles.promotedBar}>
+                <h4 className={`col-6 ${styles.promotedBarTitle}`}>HOT DEALS</h4>
+                <p className={`col-6 text-right ${styles.dots}`}>
+                  <a href='#' className={styles.dot}>
+                    <span></span>
+                  </a>
+                  <a href='#' className={styles.dot}>
+                    <span></span>
+                  </a>
+                  <a href='#' className={styles.dot}>
+                    <span></span>
+                  </a>
+                </p>
+              </div>
+              <HotDealsProductBox />
             </div>
-            <HotDealsProductBox />
-          </div>
-          <div className='col-8'>
-            <PromotedProductBox />
-            <div className={styles.btns}>
-              <Button className={styles.btn}>
-                <FontAwesomeIcon icon={faAngleLeft}></FontAwesomeIcon>
-              </Button>
-              <Button className={styles.btn}>
-                <FontAwesomeIcon icon={faAngleRight}></FontAwesomeIcon>
-              </Button>
+            <div className='col-8'>
+              <PromotedProductBox />
+              <div className={styles.btns}>
+                <Button className={styles.btn}>
+                  <FontAwesomeIcon icon={faAngleLeft}></FontAwesomeIcon>
+                </Button>
+                <Button className={styles.btn}>
+                  <FontAwesomeIcon icon={faAngleRight}></FontAwesomeIcon>
+                </Button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 Promoted.propTypes = {
   promotedProductBox: PropTypes.object,
