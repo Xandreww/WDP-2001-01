1ddfd5e8e87f4915a9758676600b440c8497c908
Rado Jedro <radoslaw.jedro@gmail.com>
2020-01-29 00:18:33 +0000

    Update props

--- src/components/common/PromotedProductBox/PromotedProductBox.js
+++ src/components/common/PromotedProductBox/PromotedProductBox.js
@@ -3,16 +3,22 @@ import PropTypes from 'prop-types';
 import Button from '../Button/Button';
 import styles from './PromotedProductBox.module.scss';
 
-const PromotedProductBox = ({ promotedProductBox }) => {
+const PromotedProductBox = ({
+  image,
+  imageName,
+  softTitle,
+  strongTitle,
+  description,
+}) => {
   return (
     <div className={styles.root}>
-      <img src={promotedProductBox.image} alt={promotedProductBox.imageName} />
+      <img src={image} alt={imageName} />
       <div className={styles.text}>
         <h3 className={styles.title}>
-          {promotedProductBox.softTitle}
-          <strong> {promotedProductBox.strongTitle}</strong>
+          {softTitle}
+          <strong> {strongTitle}</strong>
         </h3>
-        <p className={styles.description}>{promotedProductBox.description}</p>
+        <p className={styles.description}>{description}</p>
       </div>
       <Button className={styles.mainBtn}>Shop now</Button>
     </div>
