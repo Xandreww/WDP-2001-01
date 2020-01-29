fd20c32848fd18a090bd32df0061eab2766838aa
Rado Jedro <radoslaw.jedro@gmail.com>
2020-01-29 00:20:18 +0000

    Delete PromotedProductBoxContainer.js

--- src/components/common/PromotedProductBox/PromotedProductBoxContainer.js
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux';
-
-import PromotedProductBox from './PromotedProductBox';
-
-const mapStateToProps = state => ({
-  promotedProductBox: state.promotedProductBox,
-});
-
-export default connect(mapStateToProps)(PromotedProductBox);
