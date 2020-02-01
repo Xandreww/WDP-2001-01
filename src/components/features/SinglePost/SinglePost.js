911d506a18b36ea6fd6a2eb03cba025997354c8f
Andrzej Popek <andrzejpopek93@gmail.com>
2020-02-01 21:44:53 +0100

    add heading styles, add border

--- src/components/features/SinglePost/SinglePost.js
+++ src/components/features/SinglePost/SinglePost.js
@@ -5,9 +5,12 @@ import PropTypes from 'prop-types';
 const SinglePost = ({ blog }) => (
   <div className={styles.root}>
     <div className='container'>
-      <div className={'row'}>
-        <div className={'col'}>
-          <h2>{blog[0].title}</h2>
+      <div className={styles.panelBar}>
+        <div className='row no-gutters align-items-end'>
+          <div className={'col-auto ' + styles.heading}>
+            <h3>{blog[0].title}</h3>
+          </div>
+          <div className={'col'}></div>
         </div>
       </div>
       <div className={'row ' + styles.imagePart}>
@@ -17,33 +20,35 @@ const SinglePost = ({ blog }) => (
         <p className={styles.postAuthor}>{blog[0].author}</p>
         <p className={styles.postDate}>{blog[0].date}</p>
       </div>
-      <div className={'row'}>
-        <div className={'col'}>
-          <p>Content...</p>
-          <p>{blog[0].sampleText}</p>
+      <div className={styles.border}>
+        <div className={'row'}>
+          <div className={'col'}>
+            <p>Content...</p>
+            <p>{blog[0].sampleText}</p>
+          </div>
         </div>
-      </div>
-      <div className={'row'}>
-        <div className={'col'}>
-          <h2>Comments:</h2>
+        <div className={'row'}>
+          <div className={'col'}>
+            <h3>Comments:</h3>
+          </div>
         </div>
-      </div>
-      <div className={'row'}>
-        <div className={'col'}>
-          <p>{blog[0].comments[0].comment1}</p>
-          <h3>{blog[0].commentAuthors[0].author1}</h3>
-        </div>
-        <div className={'col'}>
-          <p>{blog[0].comments[1].comment2}</p>
-          <h3>{blog[0].commentAuthors[1].author2}</h3>
-        </div>
-        <div className={'col'}>
-          <p>{blog[0].comments[2].comment3}</p>
-          <h3>{blog[0].commentAuthors[2].author3}</h3>
-        </div>
-        <div className={'col'}>
-          <p>{blog[0].comments[3].comment4}</p>
-          <h3>{blog[0].commentAuthors[3].author4}</h3>
+        <div className={'row'}>
+          <div className={'col'}>
+            <p>{blog[0].comments[0].comment1}</p>
+            <h4>{blog[0].commentAuthors[0].author1}</h4>
+          </div>
+          <div className={'col'}>
+            <p>{blog[0].comments[1].comment2}</p>
+            <h4>{blog[0].commentAuthors[1].author2}</h4>
+          </div>
+          <div className={'col'}>
+            <p>{blog[0].comments[2].comment3}</p>
+            <h4>{blog[0].commentAuthors[2].author3}</h4>
+          </div>
+          <div className={'col'}>
+            <p>{blog[0].comments[3].comment4}</p>
+            <h4>{blog[0].commentAuthors[3].author4}</h4>
+          </div>
         </div>
       </div>
     </div>
