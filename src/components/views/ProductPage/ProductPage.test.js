c142f9a73b947021e293e77733134aab25a82eae
Jacek Kącki <jkacki@gmail.com>
2020-02-01 17:31:34 +0100

    WDP200101-24v2

--- src/components/views/ProductPage/ProductPage.test.js
+++ src/components/views/ProductPage/ProductPage.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import ProductPage from './ProductPage';
+import { render } from 'enzyme';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+import ErrorNotFound from '../ErrorNotFound/ErrorNotFound';
 
-describe('ProductPage', () => {
-  it('renders without crashing', () => {
-    shallow(<ProductPage />);
-  });
+test('redirects to page not found', () => {
+  const route = '/not-found';
+  const history = createMemoryHistory({ initialEntries: [route] });
+  render(
+    <Router history={history}>
+      <ErrorNotFound to={route} />
+    </Router>
+  );
+  expect(history.location.pathname).toBe('/not-found');
 });
