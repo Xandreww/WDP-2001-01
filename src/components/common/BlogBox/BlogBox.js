44213b09516eca33f35b91995c0d2def63e3bd9a
PawelJadach <pawel.jadach.96@gmail.com>
2020-02-02 19:25:46 +0100

    add route for single blog

--- src/components/common/BlogBox/BlogBox.js
+++ src/components/common/BlogBox/BlogBox.js
@@ -2,11 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './BlogBox.module.scss';
 import Button from '../Button/Button';
+import { Link } from 'react-router-dom';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faComments, faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
 
-const BlogBox = ({ image, imageName, text, date, comments }) => {
+const BlogBox = ({ image, imageName, sampleText, date, comments, id }) => {
   return (
     <div>
       <div className={styles.root}>
@@ -26,15 +27,17 @@ const BlogBox = ({ image, imageName, text, date, comments }) => {
                   icon={faComments}
                   className={styles.icon}
                 ></FontAwesomeIcon>{' '}
-                {comments}
+                {comments.length}
               </div>
             </div>
           </div>
           <div className={styles.postDetails}>
             <h3 className={styles.postDetailsHeader}>Products That Fight Static</h3>
-            <p>{text}</p>
+            <p>{sampleText}</p>
             <div>
-              <Button className={styles.btn}>Read More</Button>
+              <Link to={`/blog/${id}`}>
+                <Button className={styles.btn}>Read More</Button>
+              </Link>
             </div>
           </div>
         </div>
@@ -46,9 +49,10 @@ const BlogBox = ({ image, imageName, text, date, comments }) => {
 BlogBox.propTypes = {
   image: PropTypes.number,
   imageName: PropTypes.string,
-  text: PropTypes.string,
+  sampleText: PropTypes.string,
   date: PropTypes.string,
-  comments: PropTypes.string,
+  comments: PropTypes.array,
+  id: PropTypes.number,
 };
 
 export default BlogBox;
