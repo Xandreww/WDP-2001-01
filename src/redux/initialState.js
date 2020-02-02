ad07ec33c83c6ce4f69c1a14febdc302a50ae5e0
PawelJadach <pawel.jadach.96@gmail.com>
2020-02-02 19:48:25 +0100

    change initial state

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1380,30 +1380,101 @@ const initialState = {
       image: clientPhoto4,
     },
   ],
-  blogBox1: {
-    image: image14,
-    imageName: 'image',
-    date: '18 Nov 2018',
-    comments: ' 5 Comments',
-    text:
-      'Wherry Cat onine tails draft league line Jack Ketch Nelsons folly Sink me lanyard case shot. Gabion bowsprit bring a spring upon her cable port pink hardtack yawl gunwalls starboard ahoy. Execution dock dead men tell no tales barkadeer yo-ho-ho poop deck barque weigh anchor sheet maroon bilge water.',
-  },
-  blogBox2: {
-    image: image20,
-    imageName: 'image',
-    date: '12 Dec 2017',
-    comments: ' 6 Comments',
-    text:
-      'Splice the main brace yu Shiver me timbers Jack Ketch long clothes poop deck scuttle Sail ho schooner. Salmagundi holystone log hulk skysail lugsail cutlass Admiral of the Black gangplank long clothes. Pressgang hogshead Gold Road scuttle heave down jack loot spyglass clipper rutters sheet maroon bilge.',
-  },
-  blogBox3: {
-    image: image18,
-    imageName: 'image',
-    date: '12 Jan 2020',
-    comments: ' 4 Comments',
-    text:
-      'Starboard hempen bilge gally no prey, no pay mutiny deadlights dance the hempen jig Arr nipperkin. Square-rigged jury mast rigging grog blossom Arr cackle fruit nipper weigh anchor sutler hardtack. Swab hearties cackle fruit cutlass grog blossom man-of-war barkadeer dead men tell no tales schooner cog. ',
-  },
+  blogs: [
+    {
+      id: 0,
+      image: image14,
+      imageName: 'image-14',
+      title: 'Comfortable beds',
+      author: 'John Doe',
+      date: '18 Nov 2018',
+      sampleText:
+        'Wherry Cat onine tails draft league line Jack Ketch Nelsons folly Sink me lanyard case shot. Gabion bowsprit bring a spring upon her cable port pink hardtack yawl gunwalls starboard ahoy. Execution dock dead men tell no tales barkadeer yo-ho-ho poop deck barque weigh anchor sheet maroon bilge water.',
+      fullText: `Curabitur sodales a dolor id commodo. Donec finibus tincidunt eros sit amet rutrum. Curabitur varius odio id commodo pulvinar. Donec ac sollicitudin metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque quis sagittis libero. Maecenas vehicula ornare facilisis. Aenean lobortis, augue gravida laoreet tincidunt, libero tortor dapibus risus, vel suscipit orci tortor vel nisi. Nam congue turpis vitae mi porttitor pretium. Pellentesque rutrum eros eget sapien mollis consectetur. Fusce hendrerit fermentum purus. Aenean scelerisque lacus risus, ac tincidunt urna euismod in. Morbi vestibulum velit non lorem feugiat, sed laoreet elit molestie.....`,
+      comments: [
+        {
+          id: 0,
+          content:
+            'Suspendisse imperdiet in magna lacinia hendrerit. Proin tincidunt molestie tincidunt.',
+          author: 'Jan Nowak',
+        },
+        {
+          id: 1,
+          content:
+            'Suspendisse imperdiet in magna lacinia hendrerit. Proin tincidunt molestie tincidunt.',
+          author: 'Jan Nowak',
+        },
+        {
+          id: 2,
+          content:
+            'Suspendisse imperdiet in magna lacinia hendrerit. Proin tincidunt molestie tincidunt.',
+          author: 'Jan Nowak',
+        },
+      ],
+    },
+    {
+      id: 1,
+      image: image20,
+      imageName: 'image-20',
+      title: 'Comfortable beds',
+      author: 'John Doe',
+      date: '12 Dec 2017',
+      sampleText:
+        'Wherry Cat onine tails draft league line Jack Ketch Nelsons folly Sink me lanyard case shot. Gabion bowsprit bring a spring upon her cable port pink hardtack yawl gunwalls starboard ahoy. Execution dock dead men tell no tales barkadeer yo-ho-ho poop deck barque weigh anchor sheet maroon bilge water.',
+      fullText: `Curabitur sodales a dolor id commodo. Donec finibus tincidunt eros sit amet rutrum. Curabitur varius odio id commodo pulvinar. Donec ac sollicitudin metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque quis sagittis libero. Maecenas vehicula ornare facilisis. Aenean lobortis, augue gravida laoreet tincidunt, libero tortor dapibus risus, vel suscipit orci tortor vel nisi. Nam congue turpis vitae mi porttitor pretium. Pellentesque rutrum eros eget sapien mollis consectetur. Fusce hendrerit fermentum purus. Aenean scelerisque lacus risus, ac tincidunt urna euismod in. Morbi vestibulum velit non lorem feugiat, sed laoreet elit molestie.....`,
+      comments: [
+        {
+          id: 0,
+          content:
+            'Suspendisse imperdiet in magna lacinia hendrerit. Proin tincidunt molestie tincidunt.',
+          author: 'Jan Nowak',
+        },
+        {
+          id: 1,
+          content:
+            'Suspendisse imperdiet in magna lacinia hendrerit. Proin tincidunt molestie tincidunt.',
+          author: 'Jan Nowak',
+        },
+        {
+          id: 2,
+          content:
+            'Suspendisse imperdiet in magna lacinia hendrerit. Proin tincidunt molestie tincidunt.',
+          author: 'Jan Nowak',
+        },
+      ],
+    },
+    {
+      id: 2,
+      image: image18,
+      imageName: 'image-18',
+      title: 'Comfortable beds',
+      author: 'John Doe',
+      date: '18 Nov 2018',
+      sampleText:
+        'Wherry Cat onine tails draft league line Jack Ketch Nelsons folly Sink me lanyard case shot. Gabion bowsprit bring a spring upon her cable port pink hardtack yawl gunwalls starboard ahoy. Execution dock dead men tell no tales barkadeer yo-ho-ho poop deck barque weigh anchor sheet maroon bilge water.',
+      fullText: `Curabitur sodales a dolor id commodo. Donec finibus tincidunt eros sit amet rutrum. Curabitur varius odio id commodo pulvinar. Donec ac sollicitudin metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque quis sagittis libero. Maecenas vehicula ornare facilisis. Aenean lobortis, augue gravida laoreet tincidunt, libero tortor dapibus risus, vel suscipit orci tortor vel nisi. Nam congue turpis vitae mi porttitor pretium. Pellentesque rutrum eros eget sapien mollis consectetur. Fusce hendrerit fermentum purus. Aenean scelerisque lacus risus, ac tincidunt urna euismod in. Morbi vestibulum velit non lorem feugiat, sed laoreet elit molestie.....`,
+      comments: [
+        {
+          id: 0,
+          content:
+            'Suspendisse imperdiet in magna lacinia hendrerit. Proin tincidunt molestie tincidunt.',
+          author: 'Jan Nowak',
+        },
+        {
+          id: 1,
+          content:
+            'Suspendisse imperdiet in magna lacinia hendrerit. Proin tincidunt molestie tincidunt.',
+          author: 'Jan Nowak',
+        },
+        {
+          id: 2,
+          content:
+            'Suspendisse imperdiet in magna lacinia hendrerit. Proin tincidunt molestie tincidunt.',
+          author: 'Jan Nowak',
+        },
+      ],
+    },
+  ],
 
   promotedProductBox: {
     image: image9,
