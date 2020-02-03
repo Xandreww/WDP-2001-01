2c3e6a3999fc036b6532cc7689708f3af8274e6f
Andrzej Popek <andrzejpopek93@gmail.com>
2020-02-03 18:12:06 +0100

    add useEffect

--- src/components/views/BlogPage/BlogPage.js
+++ src/components/views/BlogPage/BlogPage.js
@@ -1,73 +1,77 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './BlogPage.module.scss';
 import PropTypes from 'prop-types';
 
-const BlogPage = ({ blogs, match }) => (
-  <div className={styles.root}>
-    <div className='container'>
-      <div className={styles.panelBar}>
-        <div className='row no-gutters align-items-end'>
-          <div className={'col-auto ' + styles.heading}>
-            <h3>
-              <span>About: </span>
-              {blogs[match.params.blogId].title}
-            </h3>
-          </div>
-          <div className={'col'}></div>
-        </div>
-      </div>
-      <div className={'row ' + styles.imagePart}>
-        <div className={'col'}>
-          <img
-            className={styles.image}
-            src={blogs[match.params.blogId].image}
-            alt={blogs[match.params.blogId].title}
-          />
-        </div>
-        <p className={styles.postAuthor}>{blogs[match.params.blogId].author}</p>
-        <p className={styles.postDate}>{blogs[match.params.blogId].date}</p>
-      </div>
-      <div className={styles.border}>
-        <div className={'row ' + styles.fullTextRow}>
-          <div className={'col'}>
-            <p className={styles.fullTextP}>{blogs[match.params.blogId].fullText}</p>
+const BlogPage = ({ blogs, match }) => {
+  /* to affect scroll down afer link direct */
+  useEffect(() => window.scrollTo(0, 0), []);
+  return (
+    <div className={styles.root}>
+      <div className='container'>
+        <div className={styles.panelBar}>
+          <div className='row no-gutters align-items-end'>
+            <div className={'col-auto ' + styles.heading}>
+              <h3>
+                <span>About: </span>
+                {blogs[match.params.blogId].title}
+              </h3>
+            </div>
+            <div className={'col'}></div>
           </div>
         </div>
-        <div className={'row'}>
+        <div className={'row ' + styles.imagePart}>
           <div className={'col'}>
-            <h3 className={styles.commentsHeader}>Comments:</h3>
+            <img
+              className={styles.image}
+              src={blogs[match.params.blogId].image}
+              alt={blogs[match.params.blogId].title}
+            />
           </div>
+          <p className={styles.postAuthor}>{blogs[match.params.blogId].author}</p>
+          <p className={styles.postDate}>{blogs[match.params.blogId].date}</p>
         </div>
-        <div className={'row'}>
-          <div className={'col'}>
-            <p className={styles.commentsContent}>
-              {blogs[match.params.blogId].comments[0].content}
-            </p>
-            <h4 className={styles.commentsAuthor}>
-              {blogs[match.params.blogId].comments[0].author}
-            </h4>
+        <div className={styles.border}>
+          <div className={'row ' + styles.fullTextRow}>
+            <div className={'col'}>
+              <p className={styles.fullTextP}>{blogs[match.params.blogId].fullText}</p>
+            </div>
           </div>
-          <div className={'col'}>
-            <p className={styles.commentsContent}>
-              {blogs[match.params.blogId].comments[1].content}
-            </p>
-            <h4 className={styles.commentsAuthor}>
-              {blogs[match.params.blogId].comments[1].author}
-            </h4>
+          <div className={'row'}>
+            <div className={'col'}>
+              <h3 className={styles.commentsHeader}>Comments:</h3>
+            </div>
           </div>
-          <div className={'col'}>
-            <p className={styles.commentsContent}>
-              {blogs[match.params.blogId].comments[2].content}
-            </p>
-            <h4 className={styles.commentsAuthor}>
-              {blogs[match.params.blogId].comments[2].author}
-            </h4>
+          <div className={'row'}>
+            <div className={'col'}>
+              <p className={styles.commentsContent}>
+                {blogs[match.params.blogId].comments[0].content}
+              </p>
+              <h4 className={styles.commentsAuthor}>
+                {blogs[match.params.blogId].comments[0].author}
+              </h4>
+            </div>
+            <div className={'col'}>
+              <p className={styles.commentsContent}>
+                {blogs[match.params.blogId].comments[1].content}
+              </p>
+              <h4 className={styles.commentsAuthor}>
+                {blogs[match.params.blogId].comments[1].author}
+              </h4>
+            </div>
+            <div className={'col'}>
+              <p className={styles.commentsContent}>
+                {blogs[match.params.blogId].comments[2].content}
+              </p>
+              <h4 className={styles.commentsAuthor}>
+                {blogs[match.params.blogId].comments[2].author}
+              </h4>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 BlogPage.propTypes = {
   blogs: PropTypes.arrayOf(
